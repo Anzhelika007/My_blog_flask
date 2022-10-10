5fa470ccb6b13be2f1eb5cb33b05f1b6bca172c2
Anzhalika Turlak <turlak.by@gmail.com>
2022-10-10 18:26:50 +0300

    add file project

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/blog')
+def index():
+   return render_template('index.html')
+
+@app.route('/contact')
+def contact():
+   return render_template('contact.html')
+
+@app.route('/register')
+def register():
+   return render_template('register.html')
+
+@app.route('/login')
+def login():
+   return render_template('login.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
