673a7cbd70bae7618e18218bab335b1f8c7152e9
Anzhalika Turlak <turlak.by@gmail.com>
2022-10-10 22:45:49 +0300

    add table sql

--- app.py
+++ app.py
@@ -1,29 +1,61 @@
 from flask import Flask, render_template, url_for
-
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+db = SQLAlchemy(app)
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), unique=True, nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+    text = db.Column(db.Text)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+    last_modified = db.Column(db.DateTime, default=datetime.utcnow)
+    category = db.Column(db.String(20), nullable=False)
+    cover = db.Column(db.String(50), nullable=False, default='default.jpg')
+
+    def __repr__(self):
+        return '<Article %r>' % self.id
+
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<Category %r>' % self.id
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String(100), nullable=False)
+    body = db.Column(db.String(300), nullable=False)
+    created_on = db.Column(db.DateTime, default=datetime.utcnow)
 
 
 @app.route('/')
 @app.route('/blog')
 def index():
-   return render_template('index.html')
+    return render_template('index.html')
+
 
 @app.route('/contact')
 def contact():
-   return render_template('contact.html')
+    return render_template('contact.html')
+
 
 @app.route('/register')
 def register():
-   return render_template('register.html')
+    return render_template('register.html')
+
 
 @app.route('/login')
 def login():
-   return render_template('login.html')
-
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
