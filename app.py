f6796050c248d14ae6d74cb4fad914b33a008418
Anzhalika Turlak <turlak.by@gmail.com>
2022-10-16 22:10:04 +0300

    add html and css page about

--- app.py
+++ app.py
@@ -52,6 +52,11 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
